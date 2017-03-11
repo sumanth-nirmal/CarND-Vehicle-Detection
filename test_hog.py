10adcf09a80f756f6543602bf3e3b5dd5184baf5
sumanth <sumanth.724@gmail.com>
2017-03-11 04:37:02 -0500

    added out hog images

--- test_hog.py
+++ test_hog.py
@@ -28,4 +28,57 @@ for fname in images:
     plt.clf()
     plt.imshow(dst)
     cv2.imshow('img',dst)
-    plt.savefig('output_images/test' + str(count) + '.png')
+    plt.savefig('output_images/test' + str(count) + '.jpg')
+
+# hog features on test images
+# load the data
+cars = glob.glob('vehicles/*/*/*.png')
+nocars = glob.glob('non-vehicles/*/*/*.png')
+
+img1 = cv2.imread(cars[10])
+img2 = cv2.imread(nocars[10])
+
+# hog parameters
+orient = 9  # HOG orientations
+pix_per_cell = 8  # HOG pixels per cell
+cell_per_block = 2  # HOG cells per block
+
+# car image
+plt.clf()
+plt.imshow(img1)
+plt.savefig('output_images/hog/input1' + '.jpg')
+
+ft, him = hog_features.fetchHOGFeatures(img1[:,:, 0], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+plt.clf()
+plt.imshow(him)
+plt.savefig('output_images/hog/output11' + '.jpg')
+
+ft, him = hog_features.fetchHOGFeatures(img1[:,:, 1], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+plt.clf()
+plt.imshow(him)
+plt.savefig('output_images/hog/output12' + '.jpg')
+
+ft, him = hog_features.fetchHOGFeatures(img1[:,:, 2], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+plt.clf()
+plt.imshow(him)
+plt.savefig('output_images/hog/output13' + '.jpg')
+
+# no car image
+plt.clf()
+plt.imshow(img2)
+plt.savefig('output_images/hog/input2' + '.jpg')
+
+ft, him = hog_features.fetchHOGFeatures(img2[:,:,0], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+plt.clf()
+plt.imshow(him)
+plt.savefig('output_images/hog/output21' + '.jpg')
+
+ft, him = hog_features.fetchHOGFeatures(img2[:,:,1], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+plt.clf()
+plt.imshow(him)
+plt.savefig('output_images/hog/output22' + '.jpg')
+
+ft, him = hog_features.fetchHOGFeatures(img2[:,:,2], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+plt.clf()
+plt.imshow(him)
+plt.savefig('output_images/hog/output23' + '.jpg')
